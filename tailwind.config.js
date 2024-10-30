52c03a3aeba370eb780d1d60261b2997cc2759f8
Juliano Lisarte <101892828+JLisarte@users.noreply.github.com>
2024-10-29 21:21:18 -0300

    feat: update styles and add font support; modify asset paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,6 +69,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'Orbitron': ['Orbitron', 'sans-serif'],
+      }
     },
   },
   plugins: [],
