c1e6c1e291452a33badde80e5889224e20373ccc
Gabriel Kunst <gabrielkt.dev@gmail.com>
2024-11-03 12:43:23 -0300

    refactor(auth): add disabeld on inputs / buttons on auth loading

--- src/app/features/auth/pages/register/register.component.ts
+++ src/app/features/auth/pages/register/register.component.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { sleep } from '@shared/utils/helpers';
 
 @Component({
   selector: 'app-register',
@@ -10,4 +11,20 @@ import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
   imports: [RouterLink, MdbFormsModule, ReactiveFormsModule, MdbRippleModule],
   templateUrl: './register.component.html',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  isSubmitting = false;
+
+  constructor() {}
+
+  async handleSubmit(event: SubmitEvent) {
+    event.preventDefault();
+
+    if (this.isSubmitting) return;
+
+    this.isSubmitting = true;
+
+    await sleep(2000);
+
+    this.isSubmitting = false;
+  }
+}
--- src/app/features/auth/pages/reset-password/reset-password.component.ts
+++ src/app/features/auth/pages/reset-password/reset-password.component.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { sleep } from '@shared/utils/helpers';
 
 @Component({
   selector: 'app-reset-password',
@@ -10,4 +11,20 @@ import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
   imports: [RouterLink, MdbFormsModule, ReactiveFormsModule, MdbRippleModule],
   templateUrl: './reset-password.component.html',
 })
-export class ResetPasswordComponent {}
+export class ResetPasswordComponent {
+  isSubmitting = false;
+
+  constructor() {}
+
+  async handleSubmit(event: SubmitEvent) {
+    event.preventDefault();
+
+    if (this.isSubmitting) return;
+
+    this.isSubmitting = true;
+
+    await sleep(2000);
+
+    this.isSubmitting = false;
+  }
+}
