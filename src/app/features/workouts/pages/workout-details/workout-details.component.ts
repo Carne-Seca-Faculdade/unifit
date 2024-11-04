9df2c1089c242178291bd5e2e4c16c5cf2721c6e
Gabriel Kunst <gabrielkt.dev@gmail.com>
2024-11-03 23:48:33 -0300

    feat(workout): add edit

--- src/app/features/workouts/pages/workout-details/workout-details.component.ts
+++ src/app/features/workouts/pages/workout-details/workout-details.component.ts
@@ -11,6 +11,7 @@ import { Workout } from '../../../../core/models/workout';
 import { Exercise } from '../../../../core/models/exercise';
 import { GlobalService } from '../../../../core/services/global.service';
 import { ExerciseListComponent } from './components/exercise-list/exercise-list.component';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-workout-details',
@@ -23,14 +24,29 @@ import { ExerciseListComponent } from './components/exercise-list/exercise-list.
     InputTextModule,
     InputNumberModule,
     FormsModule,
-    ExerciseListComponent
+    ExerciseListComponent,
+    ConfirmDialogModule,
   ],
   templateUrl: './workout-details.component.html',
 })
 export class WorkoutDetailsComponent implements OnInit, OnDestroy {
   workout!: Workout;
-  newExercise: Omit<Exercise, 'id'> = { name: '', description: '', sets: 1, reps: 1 };
+  newExercise: Omit<Exercise, 'id'> = {
+    name: '',
+    description: '',
+    sets: 1,
+    reps: 1,
+  };
+  editWorkout: Workout = {
+    id: '',
+    name: '',
+    description: '',
+    duration: 0,
+    exercises: [],
+  }
   exerciseDialogVisible = false;
+  editDialogVisible = false;
+  confirmDialogVisible = false;
   private workoutSubscription!: Subscription;
 
   constructor(
@@ -46,14 +62,17 @@ export class WorkoutDetailsComponent implements OnInit, OnDestroy {
       return;
     }
 
-    this.workoutSubscription = this.globalService.getWorkouts().subscribe(workouts => {
-      const workout = workouts.find(w => w.id === id);
-      if (workout) {
-        this.workout = workout;
-      } else {
-        this.router.navigate(['/workouts']);
-      }
-    });
+    this.workoutSubscription = this.globalService
+      .getWorkouts()
+      .subscribe((workouts) => {
+        const workout = workouts.find((w) => w.id === id);
+        if (workout) {
+          this.workout = workout;
+          this.editWorkout = { ...workout };
+        } else {
+          this.router.navigate(['/workouts']);
+        }
+      });
   }
 
   ngOnDestroy(): void {
@@ -70,17 +89,69 @@ export class WorkoutDetailsComponent implements OnInit, OnDestroy {
   }
 
   saveExercise(): void {
-    if (this.isValidExercise(this.newExercise)) {
-      this.globalService.addExercise(this.workout.id, this.newExercise);
-      this.hideExerciseDialog();
-    }
+    if (!this.isValidExercise(this.newExercise)) return;
+
+    this.globalService.addExercise(this.workout.id, this.newExercise);
+    this.hideExerciseDialog();
   }
 
   isValidExercise(exercise: Omit<Exercise, 'id'>): boolean {
-    return !!(exercise.name && exercise.description && exercise.sets && exercise.reps);
+    return !!(
+      exercise.name &&
+      exercise.description &&
+      exercise.sets &&
+      exercise.reps
+    );
   }
 
   resetNewExercise(): void {
     this.newExercise = { name: '', description: '', sets: 1, reps: 1 };
+  }
+
+  resetEditWorkout(): void {
+    this.editWorkout = {
+      id: this.workout.id,
+      name: this.workout.name,
+      description: this.workout.description,
+      duration: this.workout.duration,
+      exercises: this.workout.exercises,
+    };
+  }
+
+  showEditDialog(): void {
+    this.editDialogVisible = true;
+  }
+
+  hideEditDialog(): void {
+    this.editDialogVisible = false;
+    this.resetEditWorkout();
+  }
+
+  saveWorkout(): void {
+    const workout = this.editWorkout;
+
+    if (!this.isValidWorkout(workout)) return;
+
+    this.globalService.updateWorkout(workout!);
+    this.hideEditDialog();
+  }
+
+  isValidWorkout(workout: Workout | null): boolean {
+    if (!workout) return false;
+
+    return !!(workout.name && workout.description && workout.duration > 0);
+  }
+
+  confirmDelete(): void {
+    this.confirmDialogVisible = true;
+  }
+
+  deleteWorkout(): void {
+    this.globalService.deleteWorkout(this.workout.id);
+    this.router.navigate(['/workouts']);
+  }
+
+  onReject(): void {
+    this.confirmDialogVisible = false;
   }
 }
