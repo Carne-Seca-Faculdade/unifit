44aee03963bffd5f03b2c692897b789605bb9e6a
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2025-05-18 19:04:37 -0300

    feat: resolved user permissions according to roles

--- src/app/features/auth/services/jwt.service.ts
+++ src/app/features/auth/services/jwt.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { UserModel } from '@auth/domain/interfaces';
 import { AuthTokenService } from '@auth/services/auth-token.service';
 import { jwtDecode } from 'jwt-decode';
+import { UserRole } from '@auth/domain/enums';
 
 @Injectable({
   providedIn: 'root',
@@ -15,20 +16,31 @@ export class JWTService {
     if (!token) return null;
 
     try {
+<<<<<<< Updated upstream
       const decode: any = jwtDecode(token);
+=======
+      const decode = jwtDecode<JWTPayload>(token);
+
+      const jwtRoles = decode.realm_access?.roles ?? [];
+      const roles = jwtRoles.filter((role): role is UserRole =>
+        Object.values(UserRole).includes(role as UserRole)
+      );
+
+>>>>>>> Stashed changes
       return {
-        id: 1,
+        id: 0,
         name: decode.preferred_username,
         age: 0,
         weight: null,
         height: 0,
         email: decode.email,
-        role: decode.realm_access?.roles?.includes('ADMIN') ? 'ADMIN' : 'USER',
+        roles,
         userIdentifier: decode.sub,
+        trainingPlansIds: [],
       };
     } catch (error) {
       console.error('Erro em decodificar o token', error);
+      return null;
     }
-    return null;
   }
 }
